b63041578bff407982640dd080a50e0673acae14
Vivek Katara <vivekkatara@Viveks-MacBook-Pro.local>
2020-03-26 19:30:35 -0400

    Configuring for Heroku deployment

---
+++ client/build/precache-manifest.066b2634ec421c26dc5538ff52bd8cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317c337abce2801cbe5c4b39d35eac72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08587b0dd6d0d978965a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c55d7c6d1b546c9565a5",
+    "url": "/static/js/2.d464a7bf.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.d464a7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08587b0dd6d0d978965a",
+    "url": "/static/js/main.572f2590.chunk.js"
+  },
+  {
+    "revision": "be8263a3c1cc9c1c97ef",
+    "url": "/static/js/runtime-main.fdef84ae.js"
+  }
+]);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const calculate = require('./functions/calculate');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,6 +21,14 @@ app.post('/equal', (req, res) => {
     value: calculate.equal(req.body.numbers, req.body.operations),
   });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'build', 'index.html')); //relative path
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
